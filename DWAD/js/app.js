65c7391135f2a9dab0b08aeef5e1ea227bb93ff8
vmandic <mandic.vedran@gmail.com>
2015-01-13 02:55:14 +0100

    Updated.

--- DWAD/js/app.js
+++ DWAD/js/app.js
@@ -78,7 +78,6 @@ var app = app || {},
         // Android and Windows Phone only*
         // Triggered when the back button on the device is pressed
         onBackButton: function () {
-            alert("Back pressed!");
             console.info("APP EVENT: Back button pressed.");
 
             if (app.navigationHistory.length > 0) {
